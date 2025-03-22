f6f180bef439ee925aad25b4c48f5f0ab55cddfb
ChetanXYZ <162422064+ChetanXYZ@users.noreply.github.com>
2025-03-22 12:11:13 +0530

    Work on Javascript let and const from Odin

---
+++ js/variables_and_operators/main_page/javascript.js
@@ -0,0 +1,18 @@
+console.log("This is another test line written in an external java script file")
+
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+// const pi = 3.14;
+// pi = 10;
+
+// console.log(pi); // What will be the output?
+
+const rupee = 100;
+
+console.log(rupee);
+
+console.log((3 + 2) - 76 * (1 + 1))
