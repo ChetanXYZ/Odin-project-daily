419ff98ee68d37c02794863ade7aa42ac7756abf
ChetanXYZ <162422064+ChetanXYZ@users.noreply.github.com>
2025-04-10 13:50:07 +0530

    Worked on String methods and push code to github

---
+++ js/Data_Types_and_Conditionals/Data_types/MDN_Strings/js.js
@@ -0,0 +1,6 @@
+const string1 = "A string primitive";
+const string2 = "Also a string primitive";
+const string3 = "Yet another string primitive";
+const string4 = string1 + "\n" + string2 + "\n"+ string3; 
+
+console.log(string4);
