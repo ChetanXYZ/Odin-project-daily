a974449717d91aaaeb3033f85318461ef5f38f77
ChetanXYZ <162422064+ChetanXYZ@users.noreply.github.com>
2025-05-08 14:33:06 +0530

    Push practice code to git hub

---
+++ js/variables_and_operators/variables_in_javaScript/practice3/javaScript.js
@@ -0,0 +1,87 @@
+console.log("This text is from an external JavaScript script");
+console.log("Practice code from Odin project main page on variables and operators");
+// This is a comment or an example of a comment //
+
+// let name = "John";
+// let surname = "Doe";
+
+// console.log(name);
+// console.log(surname);
+// console.log(name, surname);
+// console.log(name + surname);
+// console.log(name * surname);
+
+// re-assign variables //
+
+// let age = 11;
+// console.log(age);
+
+// age = 54;
+
+// console.log(age);
+
+// // const pi = 3.14;
+// // pi = 10;
+
+// // console.log(pi);
+
+// console.log(3 + 2) - 76 * (1 + 1);
+
+// console.log(23 + 97)
+
+// console.log(33 + 91 + 28 + 88 + 72 + 54);
+
+// console.log((4 + 6 + 9) / 77);
+
+// let a = 10;
+// console.log(a);
+
+// a = 13;
+// console.log(a);
+
+// let b = 7 * a;
+// console.log(b);
+
+// const max = 57;
+// const actual = max - 13;
+// const percentage = actual / max;
+// console.log(percentage);
+
+// let name = "Albert";
+// console.log(name);
+
+// const birthYear = 1992;
+// console.log(birthYear);
+
+// let a = 7;
+// let b = 6;
+// let sum = a + b; 
+// console.log(sum);
+
+// const x = 1;
+// console.log(x);
+
+// x = 2;
+// console.log(x);
+
+const length = 2;
+const width = 3;
+
+console.log(length * width);
+
+let x = 5;
+let y = 10;
+
+console.log(x, y);
+
+// Swap //
+
+x = 10;
+y = 5;
+
+console.log(x, y);
+
+const firstName = "Jane";
+const lastName = "Doe";
+
+console.log(firstName, lastName);
