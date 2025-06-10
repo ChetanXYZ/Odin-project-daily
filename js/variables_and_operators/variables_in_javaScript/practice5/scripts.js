805c9cf47716ea2d4e066fc5d431bec24e01e085
ChetanXYZ <162422064+ChetanXYZ@users.noreply.github.com>
2025-06-10 12:49:51 +0530

    Push practice code to github

---
+++ js/variables_and_operators/variables_in_javaScript/practice5/scripts.js
@@ -0,0 +1,52 @@
+console.log("Hi, Today");
+
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+
+console.log(age);
+
+age = 58;
+
+console.log(age);
+
+const pi = 3.14;
+
+console.log(pi);
+
+console.log((3 + 2) - 76 * (1 + 1));
+
+console.log(23 + 97)
+
+console.log(7+9+6+7+8+9);
+
+console.log(120 + 300 + 920 + 800 + 600 + 540);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+
+console.log(a);
+
+a = 13;
+
+console.log(a);
+
+let b = 7 * a;
+
+console.log(b);
+
+const max = 57;
+
+const actual = max - 13;
+
+const percentage = actual / max;
+
+console.log(percentage);
